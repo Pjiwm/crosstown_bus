0634f781eaa8a81e1fdb01c6288594e13fd51abc
Pjiwm <pim.munne@gmail.com>
2023-04-25 21:27:17 +0200

    seperate line for subs

--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,60 @@
 //! # Crosstown Bus
 //!
 //! `crosstown_bus` is an easy-to-configure bus in Rust with RabbitMQ for event-driven systems.
-pub mod tools;
-mod queue_bus;
-mod message_handler;
 mod broadcast_publisher;
 mod broadcast_subscriber;
 mod common;
+mod message_handler;
+mod queue_bus;
+pub mod tools;
 
 use std::cell::Cell;
 use std::collections::HashMap;
 
-use std::{error::Error, cell::RefCell};
+use std::{cell::RefCell, error::Error};
 
 use amiquip::Connection;
-use broadcast_subscriber::{SubscriptionManager, BroadcastSubscriber};
-use queue_bus::{QueuePublisher, QueueListener};
-pub use common::QueueProperties;
-pub use message_handler::{MessageHandler, Subscriber};
-pub use message_handler::HandleError;
 pub use broadcast_publisher::BroadcastPublisher;
+use broadcast_subscriber::{BroadcastSubscriber, SubscriptionManager};
+pub use common::QueueProperties;
+pub use message_handler::HandleError;
+pub use message_handler::MessageHandler;
+pub use message_handler::Subscriber;
+use queue_bus::{QueueListener, QueuePublisher};
 
 pub struct CrosstownBus();
 
 impl CrosstownBus {
     pub fn new_queue_listener(url: String) -> Result<QueueListener, Box<dyn Error>> {
         Ok(QueueListener {
-            cnn: RefCell::new(Connection::insecure_open(&url)?)
+            cnn: RefCell::new(Connection::insecure_open(&url)?),
         })
     }
 
     pub fn new_queue_publisher(url: String) -> Result<QueuePublisher, Box<dyn Error>> {
         Ok(QueuePublisher {
-            cnn: RefCell::new(Connection::insecure_open(&url)?)
+            cnn: RefCell::new(Connection::insecure_open(&url)?),
         })
     }
 
     pub fn new_broadcast_publisher(url: String) -> Result<BroadcastPublisher, Box<dyn Error>> {
-        Ok(BroadcastPublisher(
-            RefCell::new(Connection::insecure_open(&url)?)
-        ))
+        Ok(BroadcastPublisher(RefCell::new(Connection::insecure_open(
+            &url,
+        )?)))
     }
 
-    pub fn new_broadcast_subscriber<T>(url: String) -> Result<BroadcastSubscriber::<T>, Box<dyn Error>>
-        where T: borsh::BorshSerialize, T: borsh::BorshDeserialize {
+    pub fn new_broadcast_subscriber<T>(
+        url: String,
+    ) -> Result<BroadcastSubscriber<T>, Box<dyn Error>>
+    where
+        T: borsh::BorshSerialize,
+        T: borsh::BorshDeserialize,
+    {
         Ok(BroadcastSubscriber {
             cnn: Cell::new(Connection::insecure_open(&url)?),
-            subs_manager: SubscriptionManager::<T> { handlers_map: HashMap::default() }
+            subs_manager: SubscriptionManager::<T> {
+                handlers_map: HashMap::default(),
+            },
         })
     }
 }
